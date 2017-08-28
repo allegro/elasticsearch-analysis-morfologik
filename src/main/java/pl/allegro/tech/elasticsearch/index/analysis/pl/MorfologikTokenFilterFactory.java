8901a665a2994665ad5129f4c71b193828becbe4
Pawel Bobruk <pawel.bobruk@allegrogroup.com>
2017-08-28 11:51:03 +0200

    - support for elasticsearch 6.0.0-beta1

--- src/main/java/pl/allegro/tech/elasticsearch/index/analysis/pl/MorfologikTokenFilterFactory.java
+++ src/main/java/pl/allegro/tech/elasticsearch/index/analysis/pl/MorfologikTokenFilterFactory.java
@@ -3,14 +3,12 @@ package pl.allegro.tech.elasticsearch.index.analysis.pl;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.morfologik.MorfologikFilter;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.analysis.AbstractTokenFilterFactory;
 
 public class MorfologikTokenFilterFactory extends AbstractTokenFilterFactory {
 
-    public MorfologikTokenFilterFactory(IndexSettings indexSettings, Environment environment,
-                                        String name, Settings settings) {
+    public MorfologikTokenFilterFactory(IndexSettings indexSettings, String name, Settings settings) {
         super(indexSettings, name, settings);
     }
 
@@ -18,5 +16,4 @@ public class MorfologikTokenFilterFactory extends AbstractTokenFilterFactory {
     public TokenStream create(TokenStream tokenStream) {
         return new MorfologikFilter(tokenStream);
     }
-
 }
