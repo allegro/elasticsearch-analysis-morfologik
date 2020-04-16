672c12fb744a8ce491204b7d4a9948626ef391ba
Pawel Bobruk <pawel.bobruk@allegro.pl>
2020-04-16 14:29:01 +0200

    migration to jdk11

--- src/test/groovy/pl/allegro/tech/elasticsearch/plugin/analysis/morfologik/MorfologikPluginIntegrationTest.groovy
+++ src/test/groovy/pl/allegro/tech/elasticsearch/plugin/analysis/morfologik/MorfologikPluginIntegrationTest.groovy
@@ -17,8 +17,8 @@ class MorfologikPluginIntegrationTest extends Specification {
     static final String MORFOLOGIK_FILE_NAME = "elasticsearch-analysis-morfologik-${ELASTIC_VERSION}.zip"
     static final String MORFOLOGIK_PLUGIN_PATH = "build/distributions/$MORFOLOGIK_FILE_NAME"
 
-    static final URI CUSTOM_DICTIONARY_PATH = getClass().getResource("/polish-wo-brev.dict").toURI()
-    static final URI CUSTOM_DICTIONARY_PATH_META = getClass().getResource("/polish-wo-brev.info").toURI()
+    static final URI CUSTOM_DICTIONARY_PATH = MorfologikPluginIntegrationTest.class.getResource("/polish-wo-brev.dict").toURI()
+    static final URI CUSTOM_DICTIONARY_PATH_META = MorfologikPluginIntegrationTest.class.getResource("/polish-wo-brev.info").toURI()
 
     static final ElasticsearchWithPluginContainer container = new ElasticsearchWithPluginContainer("docker.elastic.co/elasticsearch/elasticsearch-oss:$ELASTIC_VERSION")
     static RestHighLevelClient elasticsearchClient
