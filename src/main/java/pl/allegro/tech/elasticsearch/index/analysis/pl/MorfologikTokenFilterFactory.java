be19c5162c0965a7e40a193cc35a92d1ac8f6a35
pawel.bobruk <pawel.bobruk@allegro.pl>
2022-09-02 16:54:25 +0200

    add support for elasticsearch 8.4.1

--- src/main/java/pl/allegro/tech/elasticsearch/index/analysis/pl/MorfologikTokenFilterFactory.java
+++ src/main/java/pl/allegro/tech/elasticsearch/index/analysis/pl/MorfologikTokenFilterFactory.java
@@ -34,7 +34,7 @@ public class MorfologikTokenFilterFactory extends AbstractTokenFilterFactory {
      * @throws IOException - exception during creating a component
      */
     public MorfologikTokenFilterFactory(IndexSettings indexSettings, Environment env, String name, Settings settings) throws IOException {
-        super(indexSettings, name, settings);
+        super(name, settings);
         dictionary = getDictionary(env, settings);
     }
 
